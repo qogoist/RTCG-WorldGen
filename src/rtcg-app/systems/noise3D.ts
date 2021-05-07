31a53872265eabe59910c3fa585a462b3c9782cc
Jonas Haller <jonas.haller.jh@gmail.com>
2021-05-08 01:44:30 +0200

    Fix mistake where noise generator was created like a billion times :D

--- src/rtcg-app/systems/noise3D.ts
+++ src/rtcg-app/systems/noise3D.ts
@@ -1,9 +1,9 @@
 import { makeNoise3D, Noise3D } from "open-simplex-noise";
 import { Vector3 } from "three";
 
-function getNoise(point: Vector3): number {
-  const noise: Noise3D = makeNoise3D(100);
+const noise: Noise3D = makeNoise3D(1000);
 
+function getNoise(point: Vector3): number {
   const value: number = (noise(point.x, point.y, point.z) + 1) * 0.5;
 
   return value;
