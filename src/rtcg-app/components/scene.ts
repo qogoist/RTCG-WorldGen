27cd832d06b441b8aeefdc35066c284b90a40594
Jonas Haller <jonas.haller.jh@gmail.com>
2021-07-22 20:42:22 +0200

    Prepare for XR

--- src/rtcg-app/components/scene.ts
+++ src/rtcg-app/components/scene.ts
@@ -1,9 +1,7 @@
-import { Color, Scene } from "three";
+import { Scene } from "three";
 
 function createScene(): Scene {
   const scene: Scene = new Scene();
-
-  scene.background = new Color(0x000000);
 
   return scene;
 }
--- src/rtcg-app/systems/renderer.ts
+++ src/rtcg-app/systems/renderer.ts
@@ -1,10 +1,11 @@
 import { WebGLRenderer } from "three";
 
 function createRenderer(): WebGLRenderer {
-  const renderer: WebGLRenderer = new WebGLRenderer({ antialias: true });
+  const renderer: WebGLRenderer = new WebGLRenderer({ antialias: true, alpha: true });
 
   renderer.physicallyCorrectLights = true;
   renderer.shadowMap.enabled = true;
+  renderer.xr.enabled = true;
 
   return renderer;
 }
