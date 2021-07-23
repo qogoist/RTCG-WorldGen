dd151d01075bbc2127fe8bee5d7b1931956adb12
Jonas Haller <jonas.haller.jh@gmail.com>
2021-07-23 11:00:59 +0200

    Initialize publish branch

--- README.md
+++
@@ -1,3 +0,0 @@
-Run `npm install` to fetch all the dependencies.
-
-`npm run` will start a dev server that can be used to view the application.
--- src/rtcg-app/components/scene.ts
+++
@@ -1,9 +0,0 @@
-import { Scene } from "three";
-
-function createScene(): Scene {
-  const scene: Scene = new Scene();
-
-  return scene;
-}
-
-export default createScene;
