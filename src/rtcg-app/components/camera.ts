eb86761bb61336cbfd149f465ef0e28b5e84f910
Jonas Haller <jonas.haller.jh@gmail.com>
2021-07-23 11:27:44 +0200

    fix bug with orbit controls

--- src/rtcg-app/components/camera.ts
+++ src/rtcg-app/components/camera.ts
@@ -3,7 +3,7 @@ import { PerspectiveCamera } from "three";
 function createCamera(): PerspectiveCamera {
   const camera: PerspectiveCamera = new PerspectiveCamera(35, 1, 0.1, 100);
 
-  camera.position.set(0, 0, 0);
+  camera.position.set(0, 0, 10);
   camera.lookAt(0, 0, -10);
   camera.up.set(0, 1, 0);
 
--- src/rtcg-app/systems/controls.ts
+++ src/rtcg-app/systems/controls.ts
@@ -1,10 +1,12 @@
-import { Camera } from "three";
+import { Camera, Vector3 } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function createOrbitControls(camera: Camera, canvas: HTMLCanvasElement): OrbitControls {
   const controls: OrbitControls = new OrbitControls(camera, canvas);
   controls.enableDamping = true;
   controls.dampingFactor = 0.05;
+
+  controls.target.add(new Vector3(0, 0, -10));
 
   return controls;
 }
